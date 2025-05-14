f340f5f441ea07e9095f2efede6681671f91c354
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-14 14:18:08 +0530

    authorization posts done and used to custom middleware

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
 require('dotenv').config()
 
 
-const express=require('express')
-const app=express() 
-const jwt=require('jsonwebtoken')
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
 
 
 app.use(express.json())
 
-const data=[
+const data = [
     {
-        username:"ajsal",
-        title:"post 1"
+        username: "ajsal",
+        title: "post 1"
     },
     {
-        username:"shahil",
-        title:"post 2"
+        username: "shahil",
+        title: "post 2"
     },
     {
-        username:"harshad",
-        title:"post 3"
+        username: "harshad",
+        title: "post 3"
     }
 ]
 
-app.get("/posts",(req,res)=>{
-   res.json(data)
+app.get("/posts",authenticationToken, (req, res) => {
+    res.json(data.filter(posts=>posts.username===req.user.name))
 })
 
-app.post("/login",(req,res)=>{
-   const username=req.body.username
-   const user={name:username}
-   const acces_token=jwt.sign(user,process.env.ACCESS_TOKKEN_SECRET)
-   res.json({acces_tokken:acces_token})
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const user = { name: username }
+    const acces_token = jwt.sign(user, process.env.ACCESS_TOKKEN_SECRET)
+    res.json({ acces_tokken: acces_token })
 })
 
-app.listen(3000,()=>{
-   console.log("server is running 3000");
+function authenticationToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+
+app.listen(3000, () => {
+    console.log("server is running 3000");
 })
